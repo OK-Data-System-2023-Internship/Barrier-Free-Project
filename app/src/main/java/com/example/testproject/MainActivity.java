c3c69132da35dc741dbb298f7e0a2651a75dca5b
TaePoong719 <just5538@gmail.com>
2023-12-27 16:04:06 +0900

    메인페이지 디자인, 상단바 제거, bottom sheet 연결

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 import com.example.testproject.databinding.ActivityMainBinding;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import android.view.View;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
@@ -16,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.textId1.setText("Nice to meet you. ViewBinding");
+        View bottomSheetView = getLayoutInflater().inflate(R.layout.bottom_sheet, null);
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(bottomSheetView);
+
+        Button button = findViewById(R.id.button_bottom_sheet);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetDialog.show();
+            }
+        });
     }
 }
