2ae2515d58ff5c766097f6316e79dfd38e5c01f1
TaePoong719 <just5538@gmail.com>
2023-12-30 14:15:52 +0900

    빌드 문제 해결 1

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -9,9 +9,21 @@ import com.example.testproject.databinding.ActivityMainBinding;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import android.view.View;
 import android.widget.Button;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.testproject.classifier.Classifier;
+import com.example.testproject.classifier.Recognition;
+import com.example.testproject.databinding.ActivityMainBinding;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Classifier classifier;
     private ActivityMainBinding binding;
 
     @Override
@@ -20,16 +32,76 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        View bottomSheetView = getLayoutInflater().inflate(R.layout.bottom_sheet, null);
-        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
-        bottomSheetDialog.setContentView(bottomSheetView);
+        init();
 
-        Button button = findViewById(R.id.button_bottom_sheet);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                bottomSheetDialog.show();
-            }
-        });
+//        View bottomSheetView = getLayoutInflater().inflate(R.layout.bottom_sheet, null);
+//        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+//        bottomSheetDialog.setContentView(bottomSheetView);
+//
+//        Button button = findViewById(R.id.button_bottom_sheet);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                bottomSheetDialog.show();
+//            }
+//        });
+
     }
+    private void init() {
+        initClassifier();
+        initView();
+    }
+
+    private void initClassifier() {
+//        try {
+        classifier = new Classifier(this);
+        Log.v(LOG_TAG, "Classifier initialized");
+//        } catch (IOException e) {
+//            Toast.makeText(this, R.string.failed_to_create_classifier, Toast.LENGTH_LONG).show();
+//            Log.e(LOG_TAG, "init(): Failed to create Classifier", e);
+//        }
+    }
+
+    private void initView() {
+        binding.btnDetect.setOnClickListener(v -> onDetectClick());
+        binding.btnClear.setOnClickListener(v -> clearResult());
+    }
+
+    private void onDetectClick() {
+        if (classifier == null) {
+            Log.e(LOG_TAG, "onDetectClick(): Classifier is not initialized");
+            return;
+        } else if (binding.fpvPaint.isEmpty()) {
+            Toast.makeText(this, R.string.please_write_a_digit, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Bitmap image = binding.fpvPaint.exportToBitmap(
+                classifier.getInputShape().getWidth(), classifier.getInputShape().getHeight()
+        );
+        Recognition result = classifier.classify(image);
+        renderResult(result);
+    }
+
+    private void renderResult(Recognition result) {
+        binding.tvPrediction.setText(String.valueOf(result.getLabel()));
+        binding.tvProbability.setText(String.valueOf(result.getConfidence()));
+        binding.tvTimecost.setText(String.format(
+                getString(R.string.timecost_value),
+                result.getTimeCost()
+        ));
+    }
+
+    private void clearResult() {
+        binding.fpvPaint.clear();
+        binding.tvPrediction.setText(R.string.empty);
+        binding.tvProbability.setText(R.string.empty);
+        binding.tvTimecost.setText(R.string.empty);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // classifier.close();
+    }
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 }
---
+++ app/src/main/java/com/example/testproject/classifier/Classifier.java
@@ -0,0 +1,132 @@
+package com.example.testproject.classifier;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.SystemClock;
+import android.util.Log;
+import android.util.Size;
+import org.tensorflow.lite.Delegate;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.Tensor;
+import org.tensorflow.lite.gpu.GpuDelegate;
+import org.tensorflow.lite.nnapi.NnApiDelegate;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+import java.io.Closeable;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class Classifier {
+    private Delegate delegate;
+    private Interpreter interpreter;
+    private Tensor inputTensor;
+    private Tensor outputTensor;
+    private Size inputShape;
+    private int[] imagePixels;
+    private ByteBuffer imageBuffer;
+    private TensorBuffer outputBuffer;
+
+    public Classifier(Context context, Device device, int numThreads) {
+        switch (device) {
+            case CPU:
+                delegate = null;
+                break;
+            case NNAPI:
+                delegate = new NnApiDelegate();
+                break;
+            case GPU:
+                delegate = new GpuDelegate();
+                break;
+        }
+
+        try {
+            interpreter = new Interpreter(
+                    FileUtil.loadMappedFile(context, MODEL_FILE_NAME),
+                    new Interpreter.Options().setNumThreads(numThreads).addDelegate(delegate)
+            );
+        } catch (IOException e) {
+            // IOException 처리
+            e.printStackTrace();
+            // 필요한 추가적인 예외 처리 코드
+        }
+        inputTensor = interpreter.getInputTensor(0);
+        outputTensor = interpreter.getOutputTensor(0);
+        inputShape = new Size(inputTensor.shape()[2], inputTensor.shape()[1]);
+        imagePixels = new int[inputShape.getHeight() * inputShape.getWidth()];
+        imageBuffer = ByteBuffer.allocateDirect(4 * inputShape.getHeight() * inputShape.getWidth());
+        imageBuffer.order(ByteOrder.nativeOrder());
+        outputBuffer = TensorBuffer.createFixedSize(outputTensor.shape(), outputTensor.dataType());
+
+//        Log.v(LOG_TAG, "[Input] shape = " + inputTensor.shape().contentToString() +
+//                ", dataType = " + inputTensor.dataType());
+//        Log.v(LOG_TAG, "[Output] shape = " + outputTensor.shape().contentToString() +
+//                ", dataType = " + outputTensor.dataType());
+    }
+    public Classifier(Context context) {
+        this(context, Device.CPU, 4);
+    }
+
+    public Recognition classify(Bitmap image) {
+        convertBitmapToByteBuffer(image);
+        long start = SystemClock.uptimeMillis();
+        interpreter.run(imageBuffer, outputBuffer.getBuffer().rewind());
+        long end = SystemClock.uptimeMillis();
+        long timeCost = end - start;
+        float[] probs = outputBuffer.getFloatArray();
+        int top = argMax(probs);
+        Log.v(LOG_TAG, "classify(): timeCost = " + timeCost + ", top = " + top +
+                ", probs = " + java.util.Arrays.toString(probs));
+        return new Recognition(top, probs[top], timeCost);
+    }
+
+    public void close() {
+        interpreter.close();
+        if (delegate instanceof Closeable) {
+            try {
+                ((Closeable) delegate).close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void convertBitmapToByteBuffer(Bitmap bitmap) {
+        imageBuffer.rewind();
+        bitmap.getPixels(imagePixels, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        for (int i = 0; i < inputShape.getWidth() * inputShape.getHeight(); i++) {
+            int pixel = imagePixels[i];
+            imageBuffer.putFloat(convertPixel(pixel));
+        }
+    }
+
+    private float convertPixel(int color) {
+        return (255 - (((color >> 16) & 0xFF) * 0.299f
+                + ((color >> 8) & 0xFF) * 0.587f
+                + (color & 0xFF) * 0.114f)) / 255.0f;
+    }
+
+    private static final String LOG_TAG = Classifier.class.getSimpleName();
+    private static final String MODEL_FILE_NAME = "mnist.tflite";
+
+    public static int argMax(float[] array) {
+        int maxIndex = -1;
+        float maxValue = Float.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+                maxIndex = i;
+            }
+        }
+        if (maxIndex == -1) {
+            throw new IllegalArgumentException("Cannot find arg max in empty list");
+        }
+        return maxIndex;
+    }
+
+    public Size getInputShape(){
+        return inputShape;
+    }
+}
+
+
