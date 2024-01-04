22a25b151eeb8539d9e267b0304e98a5382ebb00
Kim Gaeun <imgaeun@gmail.com>
2024-01-04 13:32:44 +0900

    이체화면 1번, 2번

--- app/src/main/java/com/example/testproject/BankingActivity.java
+++ app/src/main/java/com/example/testproject/BankingActivity.java
@@ -1,6 +1,32 @@
 package com.example.testproject;
 
-public class BankingActivity {
+import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.testproject.databinding.ActivityBankingBinding;
+
+public class BankingActivity extends AppCompatActivity {
+    // 바인딩 객체 선언
+    private ActivityBankingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 바인딩 객체 초기화
+        binding = ActivityBankingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // 버튼 클릭 리스너 설정
+        binding.button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼 클릭 시 수행할 작업
+
+            }
+        });
+    }
 
 }
