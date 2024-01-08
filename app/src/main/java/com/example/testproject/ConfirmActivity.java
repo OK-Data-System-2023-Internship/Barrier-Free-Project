e00cacdcd4ede39499780959faf46eda66842b0a
Kim Gaeun <imgaeun@gmail.com>
2024-01-08 15:50:27 +0900

    이체 완료 페이지 수정

--- app/src/main/java/com/example/testproject/ConfirmActivity.java
+++ app/src/main/java/com/example/testproject/ConfirmActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.testproject.classifier.Classifier;
 import com.example.testproject.databinding.ActivityConfirmBinding;
 import com.example.testproject.databinding.ActivitySubbankingBinding;
 
@@ -14,30 +15,54 @@ public class ConfirmActivity extends AppCompatActivity {
 
     private ActivityConfirmBinding binding;
 
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private void initClassifier() {
+        classifier = new Classifier(this);
+        Log.v(LOG_TAG, "Classifier initialized");
+    }
+
+    private void changeIntent(Class changeToActivity){
+        Intent intent = new Intent(ConfirmActivity.this, changeToActivity);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 페이지 이동 확인 로그
+        Log.v("ConfirmActivity", "Moved to ConfirmActivity");
 
         // 바인딩 객체 초기화
         binding = ActivityConfirmBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // 페이지 이동 확인 로그
-        Log.v("ConfirmActivity", "Moved to ConfirmActivity");
+        initClassifier();
 
-        binding.button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /* main으로 이동하기 */
-                // 버튼 클릭 확인 로그
-                Log.v("ConfirmActivity", "이체 종료 후 메인으로 이동");
-
-                // 확인 버튼 클릭 시 MainActivity 페이지로 이동
-                // 인텐트 생성 및 MainActivity 시작
-                Intent intent = new Intent(ConfirmActivity.this, MainActivity.class);
-                startActivity(intent);
+        FingerPaintFragment fingerPaintFragment = new FingerPaintFragment(classifier, (int drawingNum) -> {
+            switch (drawingNum) {
+                case 0:
+                    // 이체 재실행
+                    changeIntent(BankingActivity.class);
+                    break;
+                case 1:
+                    // MainActivity 페이지로 이동
+                    changeIntent(MainActivity.class);
+                    break;
+                default:
+                    Log.v(LOG_TAG, "숫자를 다시 입력");
             }
         });
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.finger_paint_fragment_container, fingerPaintFragment)
+                    .commit();
+        }
+
+
     }
+
+    private Classifier classifier;
+
 }
