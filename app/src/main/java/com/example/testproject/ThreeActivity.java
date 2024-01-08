285ca7cfb873aa01c236918800105508dd7ac3dc
Kim Gaeun <imgaeun@gmail.com>
2024-01-08 11:03:17 +0900

    드로잉으로 페이지 전환 후 해당 페이지에서 메인화면 이동

--- app/src/main/java/com/example/testproject/ThreeActivity.java
+++ app/src/main/java/com/example/testproject/ThreeActivity.java
@@ -1,18 +1,41 @@
 package com.example.testproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.testproject.databinding.ActivityThreeBinding;
+import com.example.testproject.databinding.ActivityTwoBinding;
 
 
 public class ThreeActivity extends AppCompatActivity {
 
+    private ActivityThreeBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_three);
+        // 바인딩 객체 초기화
+        binding = ActivityThreeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         Log.e("ThreeAcvitity", "Moved to Three");
+
+        binding.button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* main으로 이동하기 */
+                // 버튼 클릭 확인 로그
+                Log.v("ThreeActivity", "333 메인으로 이동");
+
+                // 확인 버튼 클릭 시 MainActivity 페이지로 이동
+                // 인텐트 생성 및 MainActivity 시작
+                Intent intent = new Intent(ThreeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/testproject/TwoActivity.java
+++ app/src/main/java/com/example/testproject/TwoActivity.java
@@ -1,17 +1,40 @@
 package com.example.testproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.testproject.databinding.ActivityConfirmBinding;
+import com.example.testproject.databinding.ActivityTwoBinding;
+
 public class TwoActivity extends AppCompatActivity {
+
+    private ActivityTwoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_two);
+        // 바인딩 객체 초기화
+        binding = ActivityTwoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         Log.e("TwoActivity", "Moved to Two");
+
+        binding.button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* main으로 이동하기 */
+                // 버튼 클릭 확인 로그
+                Log.v("TwoActivity", "22 메인으로 이동");
+
+                // 확인 버튼 클릭 시 MainActivity 페이지로 이동
+                // 인텐트 생성 및 MainActivity 시작
+                Intent intent = new Intent(TwoActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
